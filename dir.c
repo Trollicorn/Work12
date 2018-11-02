d2f2ad49d20a2caf7232b2ea1474242abf07db76
Trollicorn <muddin3@stuy.edu>
2018-11-02 10:59:05 -0400

    added schoolwork

--- dir.c
+++ dir.c
@@ -5,6 +5,8 @@
 #include <dirent.h>
 #include <string.h>
 
+struct helper {char dirs[200]; char files[200];}
+
 int main(){
   printf("Statistics for directory: .\n");
   DIR *cur;
@@ -17,7 +19,7 @@ int main(){
     struct stat data;
     stat(entry->d_name, &data);
 
-    if (S_ISDIR(data.st_mode)){ //idk why, but it works
+    if (S_ISDIR(data.st_mode)){
       strcat(directories, entry->d_name);
       strcat(directories, "\n\t");
     }
@@ -32,4 +34,43 @@ int main(){
   printf("%s\n", directories);
   printf("%s\n", files);
   return 0;
+}
+
+char * eee(char * path){
+  printf("Statistics for directory: .\n");
+  DIR *cur;
+  cur = opendir(path);
+  struct dirent * entry = readdir(cur);
+  int size = 0;
+  char directories[200] = "Directories:\n\t";
+  char files[200] = "Files:\n\t";
+  while(entry) {
+    struct stat data;
+    stat(entry->d_name, &data);
+
+    if (S_ISDIR(data.st_mode)){
+      strcat(directories, entry->d_name);
+      strcat(directories, "\n\t");
+    }
+    else{
+      strcat(files, entry->d_name);
+      strcat(files, "\n\t");
+    }
+    size += data.st_size;
+    entry = readdir(cur);
+    return 0;
+  }
+  printf("Total Size: %d bytes\n", size);
+  printf("%s\n", directories);
+  printf("%s\n", files);
+}
+
+
+struct * helper directoy(char *path) {
+  struct *help lists = dir_helper(malloc(sizeof(struct helper)), path);
+  return lists;
+}
+
+struct * helper dir_helper(struct helper *cur; char *path) {
+  
 }
